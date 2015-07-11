edf63fa6a8e239efea059e59f736fc6d158f3481
Lau Llobet <lau.llobet@gmail.com>
2015-07-11 13:45:57 +0200

    detail

--- js/TreeRestClient.js
+++ js/TreeRestClient.js
@@ -9,14 +9,14 @@ define(['underscore', "../lib/restful", "../lib/rsvp"], function (_, restful, rs
                 .port(8080);
     }
     TreeRestClient.prototype.deleteAll = function () {
-        this.treeApi = this.api.oneUrl('articles', 'http://location.host/YOUR_PATH/trees');
+        this.treeApi = this.api.oneUrl('articles', 'http://'+location.host+'/YOUR_PATH/trees');
         console.log("delete");
         this.treeApi.delete();
     };
 
 
     TreeRestClient.prototype.put = function (tree) {
-        this.treeApi = this.api.oneUrl('articles', 'http://location.host/YOUR_PATH/trees');
+        this.treeApi = this.api.oneUrl('articles', 'http://'+location.host+'/YOUR_PATH/trees');
         var that = this;
         return new Promise(function (resolve, reject) {
             that.treeApi.put(tree).then(function (response) {
@@ -44,7 +44,7 @@ define(['underscore', "../lib/restful", "../lib/rsvp"], function (_, restful, rs
 
     TreeRestClient.prototype.get = function (x, y, dontInclude) {
         var dontIncludeString = this.buildDontIncludeString(dontInclude);
-        this.treeApi = this.api.allUrl('articles', 'http://location.host/YOUR_PATH/trees');
+        this.treeApi = this.api.allUrl('articles', 'http://'+location.host+'/YOUR_PATH/trees');
         console.log("dis:" + dontIncludeString);
         var that = this;
         return new Promise(function (resolve, reject) {
