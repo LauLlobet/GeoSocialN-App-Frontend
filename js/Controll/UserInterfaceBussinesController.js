8a66fe6d1d94fe9de1b2a4c23622c1cb155552a7
Lau Llobet <lau.llobet@gmail.com>
2015-10-24 19:02:22 +0200

    deploy at 10-24-2015 07:02:22 PM

--- js/Controll/UserInterfaceBussinesController.js
+++ js/Controll/UserInterfaceBussinesController.js
@@ -226,9 +226,9 @@ define(["../InputOutput/GpsMovmentTrigger", "../Controll/NearbyTreesFromServerTo
                     that.keyboardInterface.hideOnScene();
                     that.sceneTreeTextSetterInterface.setIsTyping(false);
                     this.gpsMovmentTrigger.setPrecisionNowIsNotImportant();
-                }).catch(function (reason) {
+                });/*.catch(function (reason) {
                     alert(reason);
-                });
+                });*/
             } else if (char === "cancel") {
                 this.state = NAVIGATE;
                 this.keyboardInterface.hideOnScene();
