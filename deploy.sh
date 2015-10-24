fe0559951ef219837d2ca0038b555b81ad66dd12
Lau Llobet <lau.llobet@gmail.com>
2015-10-24 18:40:52 +0200

    deploy at 10-24-2015

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 git add -A 
+NOW=$(date +"%m-%d-%Y")
+git commit -m "deploy at $NOW" 
 git push
 ssh -i /Users/quest/.ssh/robinKeyPair.pem ubuntu@52.24.21.196 'cd /var/lib/jetty/webapps/root/OurTreeWeb; git pull'
