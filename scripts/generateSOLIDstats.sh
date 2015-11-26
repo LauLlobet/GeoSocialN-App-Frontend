013b73d1e13bba8f1e278caf541ece16fcdc64a1
Lau Llobet <lau.llobet@gmail.com>
2015-11-26 10:42:16 +0100

    graphics are done

--- scripts/generateSOLIDstats.sh
+++ scripts/generateSOLIDstats.sh
@@ -1,24 +1,29 @@
 #ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 #brew install gawk
-cd ..;
-commitsList=( $(git --no-pager log --pretty=format:"%H") );
+#brew tap homebrew/science
+#brew update && brew upgrade
+#brew install octave
+cd ../$1
+pwd
+commitsList=( $(git --no-pager log --reverse --pretty=format:"%H") );
 echo "Starting…"
-printf "histogramT = histc([0,0],[1:30:400]);\n" > scripts/histograms.m
+printf "histogramT = histc([0,0],[1:linesgroup:linesmax]);\n" > ../scripts/histograms.m
 for i in "${commitsList[@]}"
 do
    	echo "Stats from $i"
-	git checkout $i
-	touch scripts/tmpCount
-	echo "" > scripts/tmpCount >/dev/null 2>&1
-	find ./ -type f -name "*.js" | awk '{print "wc -l "$0}' | sh | awk '{print $1}' >> scripts/tmpCount
-	linesC=($(cat scripts/tmpCount | gawk -vORS=, '{print $1}' | sed 's/,$//')) >/dev/null 2>&1
+	git checkout $i &>/dev/null
+	touch ../scripts/tmpCount
+	echo "" > ../scripts/tmpCount >/dev/null 2>&1
+	find ./ -type f -name "*.js" | awk '{print "wc -l "$0}' | sh | awk '{print $1}' >> ../scripts/tmpCount
+	linesC=($(cat ../scripts/tmpCount | gawk -vORS=, '{print $1}' | sed 's/,$//')) >/dev/null 2>&1
 	echo $linesC | md5 
-	printf "a = [-1$linesC];\nhistogramT = [ histogramT ; histc(a,[1:30:400]) ];\n" >> scripts/histograms.m
+	printf "a = [6000,$linesC];\nhistogramT = [ histogramT ; histc(a,[1:linesgroup:linesmax]) ];\n" >> ../scripts/histograms.m
 done
 
 
 
 
-#histogramT = histc([0],[1:30:400]);
+#histogramT = histc([0],[1:linesgroup:linesmax]);
 #a = [ %l ]
-#histogramT = [ histogramT ; histc(a,[1:30:400]) ]
+#histogramT = [ histogramT ; histc(a,[1:linesgroup:linesmax]) ]
+#histogram = histogramT([1:floor(size(histogramT,1)/100):size(histogramT,1)],:);
