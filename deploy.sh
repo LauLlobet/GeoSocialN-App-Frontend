026190f516dbfa11fac685658c23ca4fc8283c3e
Lau Llobet <lau.llobet@gmail.com>
2015-12-03 10:47:39 +0100

    deploy versio:debugEnabled at 12-03-2015 10:47:39 AM

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
-
+if [ -z "$1" ] && [ -z "$2" ]; then
+  echo "USAGE: ./deploy.sh versionName  [debug|nodebug]"
+  exit;
+fi
 NOW=$(date +"%m-%d-%Y %r")
 echo "# $NOW" >> cache.appcache
 git add -A 
-git commit -m "deployed at $NOW" 
+git commit -m "deploy versio:$1 at $NOW" 
 git push
 ssh -i /Users/quest/.ssh/robinKeyPair.pem ubuntu@52.35.8.50 'cd /var/lib/jetty/webapps/root/VisitTreeNumber; git pull;./publish.sh $1 $2'
 say "deployed"
